e06b677d05c658b043426aa433045aabb68ed4eb
naulkun7 <luannguyenn.2k2@gmail.com>
2023-09-13 20:10:14 +0700

    90% navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,20 +1,33 @@
 import React from "react"
 import styles from "./styles.module.scss"
 import logoImg from "../../assets/img/navbar/logo.png"
+import { Container } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 function Navbar() {
   return (
     <div className={styles.navbar}>
-      <div className={styles.leftSide}>
-        <img src={logoImg} alt="logo" />
-      </div>
-      <div className={styles.rightSide}>
-        <Link to="/">Home</Link>
-        <Link to="/">Menu</Link>
-        <Link to="/">About</Link>
-        <Link to="/">Contact</Link>
-      </div>
+      <Container>
+        <div className={styles.navbar__main}>
+          <div className={styles.navbar__logo}>
+            <img src={logoImg} alt="logo" />
+          </div>
+          <div className={styles.navbar__menu}>
+            <Link className={styles.navbar__menu__item} to="/">
+              Home
+            </Link>
+            <Link className={styles.navbar__menu__item} to="/">
+              Menu
+            </Link>
+            <Link className={styles.navbar__menu__item} to="/">
+              About
+            </Link>
+            <Link className={styles.navbar__menu__item} to="/">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </Container>
     </div>
   )
 }
